151842ddaa9a98737b03da892e501d869accbe6a
Nanda Surya <nandasurya@Nandas-MacBook-Air.local>
2023-04-04 18:12:36 +0800

    update univ

--- pages/home/UniversitasRecomend.tsx
+++ pages/home/UniversitasRecomend.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { univ } from "../data/univ";
+import { univ } from "../api/data/univ";
 
 const UniversitasRecomend = () => {
   const settings = {
