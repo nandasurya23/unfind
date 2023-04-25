b258e78228b8a89ec7ae7a3769e3ddfb431d004e
Andika611 <kadekandika611@gmail.com>
2023-04-26 00:36:44 +0800

    last

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import DetailAbout from './about/DetailAbout'
+import AboutUs from './home/AboutUs'
+import HeroSection from './home/HeroSection'
 
 const about = () => {
   return (
     <>
         <DetailAbout />
+        <AboutUs />
     </>
   )
 }
--- pages/about/DetailAbout.tsx
+++ pages/about/DetailAbout.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 const detailabout = () => {
   return (
-    <div>detailabout</div>
+    <section className="bg-blue-primary banner px-3 font-Poppins text-white">
+      <div className="container mx-auto px-3 pt-60 pb-52">
+            <h1 className="text-5xl font-bold leading-normal text-center">
+              Tentang  {" "}
+          <span className="text-yellow-primary">UNIFIND</span>{" "}
+            </h1>
+      </div>
+    </section>
   )
 }
 
